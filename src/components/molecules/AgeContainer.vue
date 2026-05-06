bdbf979b80d39e2acf1f284db7f57e9bf1a87a5a
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-06 10:26:24 +0200

    add AgeContainer and DataFieldsContainer components with styles

---
+++ src/components/molecules/AgeContainer.vue
@@ -0,0 +1,55 @@
+<script setup>
+</script>
+
+<template>
+  <ul class="age-calculation__list">
+    <li class="age-calculation__item">
+      <p class="age-calculation__value">--</p>
+      <p class="age-calculation__label">years</p>
+    </li>
+    <li class="age-calculation__item">
+      <p class="age-calculation__value">--</p>
+      <p class="age-calculation__label">months</p>
+    </li>
+    <li class="age-calculation__item">
+      <p class="age-calculation__value">--</p>
+      <p class="age-calculation__label">days</p>
+    </li>
+  </ul>
+</template>
+
+<style>
+.age-calculation__list {
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  margin-bottom: 32px;
+}
+
+.age-calculation__item {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+}
+
+.age-calculation__value {
+  font: var(--text-preset-2);
+  letter-spacing: var(--text-preset-2-letter-spacing);
+  color: var(--color-purple-500);
+}
+
+.age-calculation__label {
+  font: var(--text-preset-2);
+  letter-spacing: var(--text-preset-2-letter-spacing);
+}
+
+@media (min-width: 768px) {
+
+  .age-calculation__value,
+  .age-calculation__label {
+    font: var(--text-preset-1);
+    letter-spacing: var(--text-preset-1-letter-spacing);
+  }
+}
+</style>
---
+++ src/components/molecules/DataFieldsContainer.vue
@@ -0,0 +1,26 @@
+<script setup>
+import DataField from '../atoms/DataField.vue';
+</script>
+
+<template>
+  <ul class="data-field__list">
+    <DataField label="Day" placeholder="DD"></DataField>
+    <DataField label="Month" placeholder="MM"></DataField>
+    <DataField label="Year" placeholder="YYYY"></DataField>
+  </ul>
+</template>
+
+<style>
+.data-field__list {
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+}
+
+@media (min-width: 768px) {
+  .data-field__list {
+    justify-content: flex-start;
+    gap: 24px;
+  }
+}
+</style>
