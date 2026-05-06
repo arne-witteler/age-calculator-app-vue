a21c8be5d290da9a8165a405e2820a8c74be847e
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-06 10:26:11 +0200

    add Button and DataField components with styles

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,62 @@
+<script setup>
+</script>
+
+<template>
+  <div class="divider">
+    <button class="button">
+      <img class="button__icon" src="../../assets/images/icon-arrow.svg" alt="Calculate age" />
+    </button>
+  </div>
+</template>
+
+<style>
+.divider {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.divider::before,
+.divider::after {
+  content: "";
+  flex: 1;
+  height: 1px;
+  background-color: var(--color-grey-200);
+}
+
+.button {
+  background-color: var(--color-purple-500);
+  border-radius: 50%;
+  border: none;
+  width: 64px;
+  height: 64px;
+}
+
+.button:active {
+  background-color: var(--color-black);
+}
+
+.button__icon {
+  width: 24px;
+  height: 24px;
+}
+
+
+@media (min-width: 768px) {
+  .button {
+    width: 96px;
+    height: 96px;
+  }
+
+  .button__icon {
+    width: 44px;
+    height: 44px;
+  }
+}
+
+@media (min-width: 1024px) {
+  .divider:after {
+    flex: 0;
+  }
+}
+</style>
---
+++ src/components/atoms/DataField.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  placeholder?: string;
+}>();
+</script>
+
+<template>
+  <li class="data-field">
+    <p class="data-field__label">{{ label }}</p>
+    <input type="text" :placeholder="placeholder" class="data-field__value">
+  </li>
+</template>
+
+<style>
+.data-field {
+  display: flex;
+  flex-direction: column;
+}
+
+.data-field__label {
+  margin-bottom: 8px;
+  text-transform: uppercase;
+  font: var(--text-preset-6-bold);
+  letter-spacing: var(--text-preset-6-bold-letter-spacing);
+  color: var(--color-grey-500);
+}
+
+.data-field__value {
+  width: 88px;
+  height: 46px;
+  padding: 8px 16px;
+  border-radius: 8px;
+  border: 1px solid var(--color-grey-200);
+}
+
+.data-field__value::placeholder {
+  font: var(--text-preset-4);
+  letter-spacing: var(--text-preset-2-letter-spacing);
+  color: var(--color-grey-500);
+}
+
+@media (min-width: 768px) {
+  .data-field__label {
+    font: var(--text-preset-5-bold);
+    letter-spacing: var(--text-preset-5-bold-letter-spacing);
+  }
+
+  .data-field__value {
+    width: 170px;
+    height: 64px;
+    padding: 8px 24px;
+  }
+
+  .data-field__value::placeholder {
+    font: var(--text-preset-3);
+    letter-spacing: var(--text-preset-3-letter-spacing);
+  }
+}
+</style>
